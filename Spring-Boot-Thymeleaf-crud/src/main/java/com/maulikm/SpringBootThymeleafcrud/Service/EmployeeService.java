c42e5eca4594398f81bae1dcb9d96a78e5270cdd
Admin <Admin@DESKTOP-4SRA6L8>
2021-05-31 12:48:23 +0530

    Upload

---
+++ Spring-Boot-Thymeleaf-crud/src/main/java/com/maulikm/SpringBootThymeleafcrud/Service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.maulikm.SpringBootThymeleafcrud.Service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.maulikm.SpringBootThymeleafcrud.Model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
---
+++ Spring-Boot-Thymeleaf-crud/src/test/java/com/maulikm/SpringBootThymeleafcrud/EmployeeManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maulikm.SpringBootThymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmployeeManagementWebApplicationTests {
+
+	@Test
+	void contexLoads() {
+		
+	}
+}
