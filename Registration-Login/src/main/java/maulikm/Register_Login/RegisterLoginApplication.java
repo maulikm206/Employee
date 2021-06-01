6a587b543b85fb7226b642910b56f1d5704921f8
maulikm206 <64843971+maulikm206@users.noreply.github.com>
2021-06-01 11:18:27 +0530

    Add files via upload

---
+++ Registration-Login/src/main/java/maulikm/Register_Login/RegisterLoginApplication.java
@@ -0,0 +1,13 @@
+package maulikm.Register_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterLoginApplication.class, args);
+	}
+
+}
